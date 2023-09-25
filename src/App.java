5f1b0d84718691eef92d90fc027da3f631ed804e
ZakariyeAden <zakariye5417@gmail.com>
2023-09-25 11:48:06 -0500

    Switch Statement in Java

--- src/App.java
+++ src/App.java
@@ -37,7 +37,32 @@ public class App {
 
         int time = 20;
         String result = (time < 18) ? "Good day" : "Bad day"; // Shorthand or iternary in Java
+        System.out.println(result);
+        // #5 Switch
 
-
-    }
+        int day = 4;
+        switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+        }
+            }
 }
