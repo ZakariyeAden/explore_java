8b612c7cbcf28716d9f704c19225d85069c813a5
ZakariyeAden <zakariye5417@gmail.com>
2023-09-25 11:04:20 -0500

    Variables in Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // #1 Hello World
         System.out.println("Hello, World!");
+
+        // #2 Variables
+        String name = "Zakariye Aden";
+        // Final is like Const in Java
+        final  int age = 23;
+        
+        System.out.println("Hello " + name + " age is " + age );
     }
 }
