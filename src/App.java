0cf20d20ef05bb1b60b21357210f4cb233f7c5b3
ZakariyeAden <zakariye5417@gmail.com>
2023-09-25 11:09:48 -0500

    Adding in more Variables

--- src/App.java
+++ src/App.java
@@ -5,9 +5,15 @@ public class App {
 
         // #2 Variables
         String name = "Zakariye Aden";
+        int x, y, z;
         // Final is like Const in Java
         final  int age = 23;
-        
+        boolean myBool = true;
+        char letter = 'Z';
+
+        x = y = z = 50;
+        // Should be 150:
+        System.out.println(x + y + z);
         System.out.println("Hello " + name + " age is " + age );
     }
 }
