aea8b6e792847f49470cc5dd897e4cc556a905d0
ZakariyeAden <zakariye5417@gmail.com>
2023-09-25 11:18:50 -0500

    Learning Type Casting

--- src/App.java
+++ src/App.java
@@ -14,6 +14,14 @@ public class App {
         x = y = z = 50;
         // Should be 150:
         System.out.println(x + y + z);
+        // Name and age
         System.out.println("Hello " + name + " age is " + age );
+
+        // #3 Type Casting
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual Casting: Double to Int!
+
+        System.out.println("myDouble should be:" + myDouble); // Output: 9.78
+        System.out.println("myInt should be:" + myInt); // Output: 9
     }
 }
